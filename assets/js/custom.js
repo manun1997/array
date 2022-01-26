c4223a1249caf022d29213ac66acbbe96d6e2332
manun1997 <manu.n@dollarbirdinc.com>
2022-01-26 12:27:20 +0530

    project add

---
+++ assets/js/custom.js
@@ -0,0 +1,5 @@
+function inputarray() {
+  let outputarray = [] + document.getElementById("array").value;
+  document.getElementById("demo").innerHTML =
+    "Array Elements Are: [ " + outputarray + " ]";
+}
